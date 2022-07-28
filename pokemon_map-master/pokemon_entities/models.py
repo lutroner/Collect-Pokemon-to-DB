2644870676b5d41369b9534fbb69beaf032f94cd
lutroner <102256437+lutroner@users.noreply.github.com>
2022-07-28 13:51:39 +0300

    Add relation to db models

--- pokemon_map-master/pokemon_entities/models.py
+++ pokemon_map-master/pokemon_entities/models.py
@@ -3,6 +3,13 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
+    image = models.ImageField(blank=True)
 
     def __str__(self):
         return self.title
+
+
+class PokemonEntity(models.Model):
+    lon = models.FloatField()
+    lat = models.FloatField()
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
