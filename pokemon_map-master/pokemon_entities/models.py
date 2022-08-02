05dd91e737d9c0c3276e8cb5b48f10cbd50dc769
lutroner <102256437+lutroner@users.noreply.github.com>
2022-08-02 17:26:36 +0300

    show pokemons on mainpage map

--- pokemon_map-master/pokemon_entities/models.py
+++ pokemon_map-master/pokemon_entities/models.py
@@ -22,3 +22,6 @@ class PokemonEntity(models.Model):
     strength = models.IntegerField(default=0)
     defence = models.IntegerField(default=0)
     stamina = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.pokemon.title + ' Entity'
--- pokemon_map-master/pokemon_entities/views.py
+++ pokemon_map-master/pokemon_entities/views.py
@@ -11,6 +11,7 @@ DEFAULT_IMAGE_URL = (
     '&fill=transparent'
 )
 
+
 def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
     icon = folium.features.CustomIcon(
         image_url,
@@ -28,14 +29,14 @@ def show_all_pokemons(request):
     with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
         pokemons = json.load(database)['pokemons']
     pokemons_db = Pokemon.objects.all()
+    pokemon_entities = PokemonEntity.objects.all()
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    for pokemon in pokemons:
-        for pokemon_entity in pokemon['entities']:
-            add_pokemon(
-                folium_map, pokemon_entity['lat'],
-                pokemon_entity['lon'],
-                pokemon['img_url']
-            )
+    for pokemon in pokemon_entities:
+        add_pokemon(
+            folium_map, pokemon.lat,
+            pokemon.lon,
+            request.build_absolute_uri(pokemon.pokemon.image.url)
+        )
 
     pokemons_on_page = []
     for pokemon in pokemons_db:
