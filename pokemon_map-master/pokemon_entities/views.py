1bc5a43cc3a199261d8d7cac4305501346fce719
lutroner <102256437+lutroner@users.noreply.github.com>
2022-08-02 16:52:02 +0300

    show pokemons on main page

--- pokemon_map-master/pokemon_entities/views.py
+++ pokemon_map-master/pokemon_entities/views.py
@@ -4,14 +4,12 @@ from .models import Pokemon, PokemonEntity
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
-
 MOSCOW_CENTER = [55.751244, 37.618423]
 DEFAULT_IMAGE_URL = (
     'https://vignette.wikia.nocookie.net/pokemon/images/6/6e/%21.png/revision'
     '/latest/fixed-aspect-ratio-down/width/240/height/240?cb=20130525215832'
     '&fill=transparent'
 )
-
 
 def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
     icon = folium.features.CustomIcon(
